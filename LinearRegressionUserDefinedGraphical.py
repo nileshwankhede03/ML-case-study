ea8e2d201d42184ad9b63f928c644ac3fe920f0a
Nilesh Wankhede <114302591+nileshwankhede03@users.noreply.github.com>
2025-07-11 08:34:28 +0530

    Initial commit

---
+++ LinearRegressionUserDefinedGraphical.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def MarvellousPredictor():
+    # Load the data
+    X = [1,2,3,4,5]
+    Y = [3,4,2,4,5]
+
+    print("Values of Independent variables : ",X)
+    print("Values of Dependent variables : ",Y)
+
+    mean_X = np.mean(X)
+    mean_Y = np.mean(Y)
+
+    print("X_MEAN is : ",mean_X)
+    print("Y_MEAN is : ",mean_Y)
+
+    n = len(X)
+
+    numerator = 0
+    denomeenator = 0
+
+#   Y = mx + C
+    for i in range(n):
+        numerator = numerator + (X[i] - mean_X) * (Y[i] - mean_Y)
+        denomeenator = denomeenator + (X[i] - mean_X)**2
+
+    m = numerator / denomeenator
+
+    print("Slope of line ie m is : ",m)
+
+    c = mean_Y - (m * mean_X)
+
+    print("Y intercept of line is : ",c)
+
+    x = np.linspace(1,6,n)
+    y = c + m * x
+
+    plt.plot(x,y,color = "g", label = "Regression Line ")
+    plt.scatter(X,Y,color = 'r',label = "Scatter plot")
+
+    plt.xlabel("X : Independent variables")
+    plt.ylabel("Y : Dependent variables")
+
+    plt.legend()
+    plt.show()
+
+def main():
+    MarvellousPredictor()
+
+if __name__ == "__main__":
+    main()
